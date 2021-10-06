6290bec17723e73a172f5c505f0eed277804f2a3
Opilane TTHK <opilane@tthk.ee>
2021-10-06 11:29:36 +0300

    Add project files.

---
+++ Startclass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kordamine
+{
+    class Startclass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Tere tulemast");
+            string eesnimi = Console.ReadLine();
+            Console.WriteLine("Tere, " + eesnimi);
+            if(eesnimi.ToLower()=="Juku")
+            {
+                Console.WriteLine("Tule minu juurde külla! Lähme kinno! Kui vana sa oled?{ }",eesnimi);
+                int vanus = int.Parse(Console.ReadLine());
+                if (vanus < 0 ||  vanus>=120)
+                {
+                    Console.WriteLine("Viga andmetega!");
+
+                }
+                else if(vanus<=6)
+                {
+                    Console.WriteLine("Tasuta pilet!");
+                }
+                else if(vanus>6 && vanus<=14)
+                {
+                    Console.WriteLine("Lastepilet!");
+                }
+                else if (vanus <=15)
+                {
+                    Console.WriteLine("Täispilet!");
+                }
+                else if (vanus >65)
+                {
+                    Console.WriteLine("sooduspilet!");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Täna mind kodus pole!");
+            }
+
+            
+            int arv1 = int.Parse(Console.ReadLine());
+            int arv2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+
+
+
+            Console.WriteLine("Tere, sisestage esimene nimi");
+            string eesnimi = Console.ReadLine();
+            Console.WriteLine("Tere, sisestage teine nimi");
+            string eesnimi2 = Console.ReadLine();
+            if (eesnimi.ToLower() == "Daria" && eesnimi2.ToLower()=="natasha")
+            {
+                Console.WriteLine("Te olete pinginaabrid")
+            }
+ 
+
+
+
+
+
+                Console.ReadLine();
+        }
+    }
+}
---
+++ kusimus2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kordamine
+{
+    class kusimus2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("kirjuta cm1 sein");
+            decimal cm1 = Math.Abs(decimal.Parse(Console.ReadLine()));
+            Console.WriteLine("kirjuta cm2 sein2");
+            decimal cm2 = Math.Abs(decimal.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Põranda pindala");
+            Console.WriteLine(cm1*cm2+"cm2");
+            Console.WriteLine("Kas tahad remonti teha");
+            string otvet = Console.ReadLine();
+            if(otvet=="jah")
+            {
+                Console.WriteLine("Kui palju maksab üks suur ruut meetri hind");
+                decimal hind = decimal.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Round(hind * ((cm1/100) * (cm2/100)),2 )+ "- summa");
+            }
+            else
+            {
+                Console.WriteLine("Head aega");
+
+            }
+
+
+
+
+            Console.WriteLine("kirjuta temperatuur");
+            decimal temp = decimal.Parse(Console.ReadLine());
+            if(temp <18)
+            {
+                Console.WriteLine(temp + "see väike temperatuur");
+
+            }
+            else if(temp==18)
+            {
+                Console.WriteLine(temp + "see suur temperatuur");
+            }
+            else if (temp==18)
+            {
+                Console.WriteLine(temp + "see normaalne temperatuur");
+            }
+
+
+
+
+            Console.WriteLine("\n what is your height in centimeters");
+            int height = int.Parse(Console.ReadLine());
+            if (height<50 && height>300)
+            {
+                Console.WriteLine(temp + "error");
+
+            }
+            else if (height< 100 && height > 49)
+            {
+                Console.WriteLine(temp + "you are small");
+            }
+            else if (height <200 && height >99)
+            {
+                Console.WriteLine(temp + "you are average");
+            }
+            else
+            {
+                Console.WriteLine("You are tall");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
